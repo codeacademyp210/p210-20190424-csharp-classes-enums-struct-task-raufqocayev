1e9dafcffa06a99d22bca872e0662d2e2783f6a1
raufqocayev <46812926+raufqocayev@users.noreply.github.com>
2019-04-25 00:30:10 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+
+namespace ConsoleApp3
+{
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Student student = new Student();
+            
+            Console.Write("What is your group : ");
+            student.Group = Console.ReadLine();
+
+            Console.Write("What is your name? ");
+            student.Name = Console.ReadLine();
+
+            Console.Write("What is your surname? ");
+            student.Surname = Console.ReadLine();
+
+
+            Console.WriteLine($"Hello {student.Name} {student.Surname} your group is {student.Group}");
+
+            Console.ReadKey();
+
+
+        }
+        
+    }
+
+
+    class Group
+    {
+        public string Name { get; set; }
+
+    }
+
+    class Student
+
+    {
+        Group group = new Group();
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Group
+
+        {
+
+            get
+            {
+                return group.Name;
+            }
+
+            set
+            {
+                group.Name = value;
+            }
+        }
+    
+        
+    }
+}
